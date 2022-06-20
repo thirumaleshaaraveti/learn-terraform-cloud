c7c0f75b93c802084d0dcaa7745caff00634b73f
AARAVETI THIRUMALESH <athiruma@athiruma.remote.csb>
2022-06-20 15:43:34 +0530

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "hashicorp-learn"
+    organization = "learnt"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
